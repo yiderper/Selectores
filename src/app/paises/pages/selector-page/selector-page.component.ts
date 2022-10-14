ccb9ef980ba13899062e02b3cfae7ada54cc707b
YPerlaza <yider.per@gmail.com>
2022-10-14 12:47:41 -0500

    Selectores

---
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+})
+export class SelectorPageComponent implements OnInit {
+
+  miForm : FormGroup = this.fb.group({
+    region: ['',Validators.required]
+  })
+
+  constructor(private fb : FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  guardar(){
+    console.log(this.miForm.value)
+  }
+
+}
